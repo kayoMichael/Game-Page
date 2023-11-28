7637e082ecdc8aa1d7f8d7e2f0512a1d97631eaa
kayoMichael <michaelxkayo@hotmail.com>
2023-11-27 23:07:08 -0500

    Build Search Bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/epic-games-logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 function NavigationBar() {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px"></Image>
+      <SearchInput></SearchInput>
       <ColorModeSwitch />
     </HStack>
   );
--- src/services/img-url.ts
+++ src/services/img-url.ts
@@ -1,5 +1,6 @@
+import placeHolder from '../assets/Sample_Img.png'
 const getCroppedImageUrl = (url: string) => {
-    if (!url) return '';
+    if (!url) return placeHolder;
     const index = url.indexOf('media/') + 'meida/'.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
